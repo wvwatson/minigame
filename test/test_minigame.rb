3c1b6bb2b2e64b0305cc922846b3d6ada8baeec5
Wavell Watson <wavell.watson@gmail.com>
2013-07-12 23:41:07 -0500

    weakly dominated list test passing

--- test/test_minigame.rb
+++ test/test_minigame.rb
@@ -41,6 +41,24 @@ class MiniGameTest < Minitest::Test
     strategy_profile
   end
 
+  def new_weak_profiles
+    employer, employee = new_players
+    accept, generic, task, deny = new_strategies 
+    strategy_profile = StrategyProfile.new
+    strategy_profile << {id: 1, strategy: generic, payoff: -2, player: employer}
+    strategy_profile << {id: 1, strategy: accept, payoff: 5, player: employee}
+
+    strategy_profile << {id: 2, strategy: generic, payoff: -1, player: employer}
+    strategy_profile << {id: 2, strategy: deny, payoff: -2, player: employee}
+
+    # not realistic
+    strategy_profile << {id: 3, strategy: task, payoff: -1, player: employer}
+    strategy_profile << {id: 3, strategy: deny, payoff: -2, player: employee}
+
+    strategy_profile << {id: 4, strategy: task, payoff: 4, player: employer}
+    strategy_profile << {id: 4, strategy: accept, payoff: 1, player: employee}
+  end
+
   def test_mixin_game
     game = Game.new
     assert_equal Game, game.class
@@ -50,7 +68,7 @@ class MiniGameTest < Minitest::Test
     employer, employee = new_players
     assert_equal "IBM", employer.first[:name]
   end
-  
+
   def test_new_strategy
     strategies = new_strategies
     assert_equal 4, strategies.count
@@ -74,4 +92,9 @@ class MiniGameTest < Minitest::Test
     assert_equal [{:name=>"Generic Training"}, {:name=>"Deny Training"}], game.strictly_dominated_list
   end
 
+  def test_weakly_dominated_list
+    game = Game.new
+    game.strategy_profiles = new_profiles
+    assert_equal [{:name=>"Generic Training"}, {:name=>"Deny Training"}], game.weakly_dominated_list
+  end
 end
